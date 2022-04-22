93889b099c5daccca013659d72cb49e026d5df45
mukund30 <mukund_mr@rediff.com>
2022-04-22 12:29:12 +0530

    Update config.py
    updating my connection strings

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-customhost = "employee.coghw13fheqo.us-east-2.rds.amazonaws.com"
+customhost = "employee.cqymwpppbx79.ap-south-1.rds.amazonaws.com"
 customuser = "intellipaat"
 custompass = "intellipaat123"
 customdb = "employee"
-custombucket = "addemployee"
+custombucket = "employeecollection"
 customregion = "us-east-2"
